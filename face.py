5e7c945de83e300d11a2a2dc97e5bd256a11d106
metheus5 <51486196+metheus5@users.noreply.github.com>
2019-06-07 14:10:52 -0300

    Update face.py

--- face.py
+++ face.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageTk
 
 import tkinter.messagebox
 
-
 def exitt():
     exit()
 
@@ -27,21 +26,59 @@ def add():
 def brisa():
     wBrisa = Tk()
     wBrisa.title("BRISA")
+    wBrisa.geometry("300x200")
+    l1 = Label(wBrisa, text= "Duração: ").place(x=20, y=20)
+    e1 = Entry(wBrisa, textvar= duracao).place(x=150, y=20)  
+    
+    b0=Button(wBrisa, text="Ok", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"), command=listar)
+    b0.place(x=60, y=120)
+    b1=Button(wBrisa, text="Cancel", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"), command=wBrisa.destroy)
+    b1.place(x=180, y=120)
 
 def ripple():
     wRipple = Tk()
     wRipple.title("RIPPLE")
+    wRipple.geometry("300x200")
+    l1 = Label(wRipple, text= "Amplitude: ").place(x=20,y=20)
+    l2 = Label(wRipple, text= "Duração: ").place(x=20, y=60)
+    e1 = Entry(wRipple).place(x=150, y=20)
+    e2 = Entry(wRipple).place(x=150, y=60)
+    b0=Button(wRipple, text="Ok", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"))
+    b0.place(x=60, y=120)
+    b1=Button(wRipple, text="Cancel", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"), command=wRipple.destroy)
+    b1.place(x=180, y=120)
 
 def rajada():
     wRajada = Tk()
     wRajada.title("RAJADA")
+    wRajada.geometry("300x200")
+    l1 = Label(wRajada, text= "Velocidade final: ").place(x=20,y=20)
+    l2 = Label(wRajada, text= "Duração: ").place(x=20, y=60)
+    e1 = Entry(wRajada).place(x=150, y=20)
+    e2 = Entry(wRajada).place(x=150, y=60)
+    b0=Button(wRajada, text="Ok", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"))
+    b0.place(x=60, y=120)
+    b1=Button(wRajada, text="Cancel", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"), command=wRajada.destroy)
+    b1.place(x=180, y=120)
 
 def rampa():
     wRampa = Tk()
     wRampa.title("RAMPA")
+    wRampa.geometry("300x200")
+    l1 = Label(wRampa, text= "Velocidade final: ").place(x=20,y=20)
+    l2 = Label(wRampa, text= "Duração: ").place(x=20, y=60)
+    e1 = Entry(wRampa).place(x=150, y=20)
+    e2 = Entry(wRampa).place(x=150, y=60)
+    b0=Button(wRampa, text="Ok", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"), command= listar)
+    b0.place(x=60, y=120)
+    b1=Button(wRampa, text="Cancel", width= 8, height=1 ,fg='black', bg= 'gray', relief=GROOVE, font=("arial", 13, "italic"), command=wRampa.destroy)
+    b1.place(x=180, y=120)
 
-# def listar():
-    
+def listar():
+    aux = int(duracao.get())
+    print (aux)
+
+###################################### INÍCIO ######################################
 
 root=Tk()
 root.geometry("600x600")
@@ -50,6 +87,7 @@ root.title("Emulador de vento")
 # VARIABLES #
 baud = IntVar()
 pary = IntVar()
+duracao = IntVar()
 conf = StringVar()
 vel_ini = IntVar()
 vel_max = IntVar()
@@ -61,14 +99,10 @@ label2 = Label(root,text="BaudRate (bit/s)", font=("arial", 12, "bold")).place(x
 label3 = Label(root,text="Histórico", font=("arial", 12, "bold")).place(x=20, y=320)
 label4 = Label(root,text="Paridade", font=("arial", 12, "bold")).place(x=300, y=150)
 label5 = Label(root,text="Velocidade Inicial", font=("arial", 9)).place(x=20, y=250)
-# label6 = Label(root,text="Velocidade Final", font=("arial", 9)).place(x=20, y=320)
-# label7 = Label(root,text="Tempo de duração", font=("arial", 9)).place(x=20, y=350)
 
 # ENTRY #
 
 e1 = Entry(root, textvar= vel_ini).place(x= 130, y=250)
-# e2 = Entry(root, textvar= vel_max).place(x= 130, y=320)
-# e3 = Entry(root, textvar= tempo).place(x= 130, y=350)
 
 # COMBO BOX #
 list1 = ['BRISA', 'RAMPA', 'RIPPLE', 'RAJADA']
