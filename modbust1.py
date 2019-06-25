aa8f192804cd7ce8cd73c9957c1c031ae1a8a692
metheus5 <51486196+metheus5@users.noreply.github.com>
2019-06-24 21:21:32 -0300

    Update modbust1.py

--- modbust1.py
+++ modbust1.py
@@ -15,37 +15,54 @@ instrument.serial.timeout  = 0.5 # seconds
 instrument.serial.xonxoff = False
 # instrument.serial.rtscts = True
 instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
-print(instrument)
+# print(instrument)
 
 i = 0
-sleep_time = 5
+sleep_time = 10
 # instrument.write_register(683,900)
 
+# listinha = []
+
+#for i in range (0, 10):
+#    listinha.insert(i, i)
+# listinha = ["RAMPA", "10", "222x", "100", "RAMPA", "15", "666 CAPETAx", "170", "RAJADA", "10", "2323x", "100" ]
+
 try:
-    print(instrument.read_register(1, 0), i)
-    instrument.write_register(133,30)
-    i = i + 1
-    print(instrument.read_register(1, 0), i)
-    time.sleep(sleep_time)
-    i = i+1
-    print(instrument.read_register(1, 0), i)
-    instrument.write_register(133,10)
-    time.sleep(sleep_time)
-    i = i+1
-    print(instrument.read_register(1, 0), i)
+    # acl = (duração*velocidade_final)/(velocidade_final - velocidade_inicial)
+
+
+    time.sleep(5)
+    instrument.write_register(100,0,1)
     instrument.write_register(133,0)
+    instrument.write_register(134,1)
+
+    instrument.write_register(100,4,1)
+    durex = instrument.read_register(100, 1)
+    print(durex)
+    instrument.write_register(682,0x0017)
+
+    instrument.write_register(134,21)
+    time.sleep(0.1)
+    instrument.write_register(133,20)
+    print(instrument.read_register(2, 0))
+
     time.sleep(sleep_time)
-    i = i+1
-    print(instrument.read_register(1, 0), i)
-    instrument.write_register(133,40)
+
+    instrument.write_register(134,51)
+    time.sleep(0.05)
+    instrument.write_register(133,50)
+    print(instrument.read_register(2, 0))
+   
     time.sleep(sleep_time)
-    i = i+1
-    print(instrument.read_register(1, 0), i)
-    instrument.write_register(133,30)
-    i = i+1
-    # instrument.write_register(683,0)
-    # instrument.write_registers(100, [10,20]) # seconds multiply by 10 (1s is equal to 10) P0100(seconds)
-    # instrument.write_registers(133, [0,1800]) # P0133(velo.)
+    
+    instrument.write_register(134,76)
+    time.sleep(0.05)
+    instrument.write_register(133,75)
+    print(instrument.read_register(2, 0))
+    
+    time.sleep(sleep_time)
+    
+    instrument.write_register(682,0x0014)
 
 except IOError:
     print("Failed to read from instrument")
