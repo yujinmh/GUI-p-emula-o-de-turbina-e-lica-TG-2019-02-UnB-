37587c94cdfc42a6a06f2ed5eca4a2bcf490722f
metheus5 <51486196+metheus5@users.noreply.github.com>
2019-06-25 14:02:30 -0300

    Update modbust1.py

--- modbust1.py
+++ modbust1.py
@@ -9,23 +9,11 @@ instrument.serial.bytesize = 8
 instrument.serial.parity   = serial.PARITY_NONE
 instrument.serial.stopbits = 1
 instrument.debug = False
-# instrument.serial.dsrdtr = False
-# instrument.close_port_after_each_call = True
 instrument.serial.timeout  = 0.5 # seconds
 instrument.serial.xonxoff = False
-# instrument.serial.rtscts = True
 instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
-# print(instrument)
-
 i = 0
 sleep_time = 10
-# instrument.write_register(683,900)
-
-# listinha = []
-
-#for i in range (0, 10):
-#    listinha.insert(i, i)
-# listinha = ["RAMPA", "10", "222x", "100", "RAMPA", "15", "666 CAPETAx", "170", "RAJADA", "10", "2323x", "100" ]
 
 try:
     # acl = (duração*velocidade_final)/(velocidade_final - velocidade_inicial)
